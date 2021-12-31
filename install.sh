cc1491d65a097fca74b8d66ad0a4e2208c44f3af
Luke Fernandez <luke.matthew.fernandez@gmail.com>
2021-12-31 11:01:53 -0800

    Add gnome tweaks to dotfiles

--- install.sh
+++ install.sh
@@ -8,12 +8,24 @@ mv .vim ~/.vim
 # Mac OS
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
-	echo "Installed dotfiles for Mac OS."
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+	brew install neovim
 	mv .iterm2/plist ~/Library/Preferences/com.googlecode.iterm2.plist	
+	echo "Installed dotfiles for MacOS."
 fi
 
 # Linux
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+	sudo apt install neovim
+	
+	sudo cp -r .themes/* /usr/share/themes/
+	sudo cp -r .icons/* /usr/share/icons/
+	gsettings set org.gnome.desktop.interface icon-theme "Simply-Green-Circles"
+	gsettings set org.gnome.desktop.interface gtk-theme "Arc-Dark"
+	gsettings set org.gnome.desktop.background picture-uri file:///home/$(whoami)/dotfiles/.linux/background.jpg
+	sudo apt install gnome-tweaks
+	sudo apt install gnome-shell-extensions
+	
 	echo "Installed dotfiles for linux." 
 fi
