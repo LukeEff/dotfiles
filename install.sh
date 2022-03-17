cbd1c612d8726d3582025f12b4017d6e90fc09d9
Luke Fernandez <luke.matthew.fernandez@gmail.com>
2022-03-16 17:43:18 -0700

    fix things

--- install.sh
+++ install.sh
@@ -1,38 +1,38 @@
 #!/bin/bash
+homeFiles=(".bashrc" ".bash_login" ".bash_profile" ".profile" ".bash_login" ".vim" ".gitconfig") 
 
 # Mac OS
 if [[ "$OSTYPE" == "darwin"* ]]; then
 
 	# Variables
 	HOME=/Users/$(whoami)
-	DOTFILES=$HOME/.dotfiles
+	DOTFILES=$HOME/dotfiles
 
 	# Install 
-	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+	#/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 	brew install neovim
 	brew install --cask visual-studio-code
 	brew install --cask intellij-idea
 
-	# Bash configuration 
-	ln -s $DOTFILES/.bashrc $HOME/.bashrc
-	ln -s $DOTFILES/.bash_login $HOME/.bash_login
-	ln -s $DOTFILES/.bash_profile $HOME/.bash_profile
-	ln -s $DOTFILES/.profile $HOME/.profile
-	ln -s $DOTFILES/.bash_login $HOME/.bash_login
+	for file in ${homeFiles[@]}; do
+		rm -rf $HOME/$file
+		ln -s $DOTFILES/$file $HOME/$file
+	done
 
 	# VSCode
+	rm $HOME/Library/Application\ Support/Code/User/settings.json
+	rm $HOME/Library/Application\ Support/Code/User/keybindings.json
 	ln -s $DOTFILES/.vscode/settings.json $HOME/Library/Application\ Support/Code/User/settings.json
 	ln -s $DOTFILES/.vscode/keybindings.json $HOME/Library/Application\ Support/Code/User/keybindings.json
 
 	# Misc.
-	ln -s $DOTFILES/.vim $HOME/.vim
-	ln -s $DOTFILES/.gitconfig $HOME/.gitconfig
-	ln -s $DOTFILES/.iterm2/plist $HOME/Library/Preferences/com.googlecode.iterm2.plist/
+	rm $HOME/Library/Preferences/com.googlecode.iterm2.plist
+	ln -s $DOTFILES/.iterm2/com.googlecode.iterm2.plist $HOME/Library/Preferences/com.googlecode.iterm2.plist
 
 	echo "Installed dotfiles for MacOS."
 fi
 
-# Linux
+# Linux 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 	sudo apt install neovim
 	
