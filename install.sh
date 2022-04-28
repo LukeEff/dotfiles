5f634afc3b272df82898e1a3c736ae6bd2d2ca41
Luke Fernandez <luke.matthew.fernandez@gmail.com>
2022-04-28 13:21:53 -0700

    install: include slack

--- install.sh
+++ install.sh
@@ -18,6 +18,7 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
 	brew install node
 	brew install java
 	brew install firefox
+	brew install --cask slack
 	brew install --cask zoom
 	brew install --cask visual-studio-code
 	brew install --cask intellij-idea
